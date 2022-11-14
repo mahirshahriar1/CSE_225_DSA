d8dc896955d615eaf88547d8f2ee52b638e97d34
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2022-11-14 08:55:20 +0600

    Update main.cpp

--- Unsorted List (Linked List)/main.cpp
+++ Unsorted List (Linked List)/main.cpp
@@ -1,202 +1,85 @@
-#include <bits/stdc++.h>
-#include "UnsortedType.h"
+#include <iostream>
+#include "UnsortedType.cpp"
 using namespace std;
-template <class ItemType>
-UnsortedType<ItemType>::UnsortedType()
+
+template <class T>
+void print(UnsortedType<T> &obj)
 {
-    length = 0;
-    listData = NULL;
-    currentPos = NULL;
-}
-template <class ItemType>
-UnsortedType<ItemType>::~UnsortedType()
-{
-    MakeEmpty();
-}
-template <class ItemType>
-int UnsortedType<ItemType>::LengthIs()
-{
-    return length;
-}
-template <class ItemType>
-void UnsortedType<ItemType>::ResetList()
-{
-    currentPos = NULL;
-}
-template<class ItemType>
-bool UnsortedType<ItemType>::IsFull()
-{
-    NodeType* location;
-    try
+    obj.ResetList();
+    int n=obj.LengthIs();
+    T item;
+    while(n--)
     {
-        location = new NodeType;
-        delete location;
-        return false;
+        obj.GetNextItem(item);
+        cout<<item<<' ';
     }
-    catch(bad_alloc& exception)
+    cout<<endl;
+}
+
+template <class T>
+void retrieve(UnsortedType <int> & obj,T item)
+{
+    bool b;
+    obj.RetrieveItem(item,b);
+    if(b)
     {
-        return true;
-    }
-}
-template <class ItemType>
-// Add Item to the front
-void UnsortedType<ItemType>::InsertItemAtBeg(ItemType
-                                        item)
-{
-    NodeType* location;
-    location = new NodeType;
-    location->info = item;
-    location->next = listData;
-    listData = location;
-    length++;
-}
-
-template <class ItemType>
-// Add Item to the back
-void UnsortedType<ItemType>::InsertItemAtEnd(ItemType
-                                        item)
-{
-    NodeType* ptr= listData;
-    NodeType* prev= NULL;
-
-    NodeType* loc= new NodeType;
-
-    while(ptr!=NULL){
-        prev=ptr;
-        ptr=ptr->next;
-    }
-    
-    if(prev==NULL){
-        loc->info = item;
-        loc->next = listData;
-        listData = loc;
-    }else{
-        loc->info = item;
-        loc->next = NULL;
-        prev->next=loc;
-    }
-
-    length++;
-}
-
-template <class ItemType>
-void UnsortedType<ItemType>::DeleteItem(ItemType
-                                        item)
-{
-    // NodeType* location = listData;
-    // NodeType* tempLocation;
-    // bool found=false;
-    // if (item == listData->info)
-    // {
-    //     listData = listData->next;
-    //     delete location;
-    //     length--;
-    //     return;
-    // }
-    // else
-    // {
-    //     while (location->next!=NULL&&(item!=(location->next)->info)){
-    //         location = location->next;      
-    //     }
-    
-    //     if(location->next==NULL){           
-    //         found=false;
-    //     }
-    //     else{ 
-    //         found=true;           
-    //         location->next = location->next->next;
-    //     }
-    // }
-    // if(found){
-    //      delete location->next;
-    //      length--;
-    // }else{
-    //     cout<<item<<" is not present in the list."<<endl;
-    // }
-
-    //-------------------------------------------//
-    bool found=false;
-    NodeType* location = listData;
-    NodeType* prev=NULL;
-    if (item == listData->info)
-    {
-        listData = listData->next;
-        found=true;
+        cout<<"Item is found"<<endl;
     }
     else
     {
-        while (location!=NULL&&item!=(location->info)){
-            prev=location;
-            location = location->next;
-        }
-
-        if(location==NULL){
-            found=false;
-        }
-        else{ 
-            found=true;
-            prev->next = location->next;
-        }
+        cout<<"Item is not found"<<endl;
     }
-    if(found){
-         delete location;
-         length--;
-    }else{
-        cout<<item<<" is not present in the list."<<endl;
-    }
-      
 }
 
-template <class ItemType>
-void UnsortedType<ItemType>::MakeEmpty()
+template <class T>
+void checkfull(UnsortedType <T> &obj)
 {
-    NodeType* tempPtr;
-    while (listData != NULL)
+    bool b=obj.IsFull();
+    if(b)
     {
-        tempPtr = listData;
-        listData = listData->next;
-        delete tempPtr;
+        cout<<"List is full"<<endl;
     }
-    length = 0;
-}
-template <class ItemType>
-void
-UnsortedType<ItemType>::RetrieveItem(ItemType&
-                                     item, bool& found)
-{
-    NodeType* location = listData;
-    bool moreToSearch = (location != NULL);
-    found = false;
-    while (moreToSearch && !found)
-    {
-        if (item == location->info)
-            found = true;
-        else
-        {
-            location = location->next;
-            moreToSearch = (location != NULL);
-        }
-    }
-}
-template <class ItemType>
-void
-UnsortedType<ItemType>::GetNextItem(ItemType&
-                                    item)
-{
-    if (currentPos == NULL)
-        currentPos = listData;
     else
-        currentPos = currentPos->next;
-    item = currentPos->info;
-}
-template <class ItemType>
-void UnsortedType<ItemType>::PrintList()
-{
-    NodeType* tempPtr = listData;
-    while (tempPtr!=NULL)
     {
-        cout << tempPtr->info << " ";
-        tempPtr = tempPtr->next;
+        cout<<"List is not full"<<endl;
     }
-    return;
+}
+
+
+int main()
+{
+    UnsortedType <int> obj;
+    obj.InsertItemAtEnd(5);
+    obj.InsertItemAtEnd(7);
+    obj.InsertItemAtEnd(6);
+    obj.InsertItemAtEnd(9);
+
+    obj.PrintList();
+                cout<<endl;
+    obj.InsertItemAtEnd(1);
+
+    print(obj);
+
+    retrieve(obj,10);
+
+    checkfull(obj);
+
+    obj.DeleteItem(5);
+    checkfull(obj);
+    obj.DeleteItem(1);
+
+    print(obj);etrieve(obj,4);
+    retrieve(obj,5);
+    retrieve(obj,9);
+    retrieve(obj,10);
+
+    checkfull(obj);
+
+    obj.DeleteItem(5);
+    checkfull(obj);
+    obj.DeleteItem(1);
+
+    print(obj);
+
+    return 0;
 }
