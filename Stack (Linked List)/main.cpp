f6f599d91d4b07ad789df13600f4e074a8bc91a3
Mahir Shahriar Tamim <03mahirshahriar@gmail.com>
2022-11-30 11:37:30 +0600

    infix complete

--- Stack (Linked List)/main.cpp
+++ Stack (Linked List)/main.cpp
@@ -19,11 +19,15 @@ int precedence(char op){
     return 0;
 }
 
-int main(){
+double evaluate(char x, double a, double b)
+{
+    if     (x == '+') return a + b;    
+    else if(x == '-') return a - b;
+    else if(x == '*') return a * b;
+    else if(x == '/') return a / b;       
+}
 
-    string equation;
-    getline(cin,equation);
-
+void solve(string equation){
     string postfix="",temp="";
     bool unbalanced=false;
 
@@ -78,14 +82,69 @@ int main(){
 
     if(unbalanced){
         cout<<"Invalid Expression"<<endl;
-        return 0;
+        return;
     }
     while(!operands.IsEmpty()){
         postfix+=operands.Top();
         operands.Pop();
     }
+    //cout<<postfix<<endl;
 
-    cout<<postfix<<endl;
+    double ans=0;
+
+    StackType<double> values;
+
+    int ind=0;
+
+    while(ind<postfix.length()){
+        if(isNum(postfix[ind])){
+            string temp="";
+            while(isNum(postfix[ind])){
+                temp+=postfix[ind++];
+            }
+            int num=stoi(temp);
+            values.Push(num);
+        }
+        else if(isOperator(postfix[ind])){
+            if(values.IsEmpty()){
+                unbalanced=true;
+                break;
+            }
+            double a=values.Top();
+            values.Pop();
+            
+            if(values.IsEmpty()){
+                unbalanced=true;
+                break;
+            }
+            double b=values.Top();
+            values.Pop();
+
+            double c=evaluate(postfix[ind],b,a);
+            values.Push(c);
+            ind++;
+        }else{
+            ind++;
+        }
+    }
+    if(unbalanced or values.IsEmpty()){
+        cout<<"Invalid Expression."<<endl;
+    }else{
+        cout<<values.Top()<<endl;
+    }
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE  
+           freopen("input.txt", "r", stdin); 
+           freopen("output.txt", "w",stdout);
+    #endif
+    string line;
+    while(getline(cin,line)){
+        solve(line);
+    }
+    
 
     return 0;
 }
