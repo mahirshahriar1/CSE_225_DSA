2fb58f14e664c6cff078a6b74276ab71602202dc
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2022-12-24 10:19:20 +0600

    Add files via upload

---
+++ Final Exam/question 2/2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "src/graphtype.cpp"
+using namespace std;
+
+template <class T>
+void printAdj(GraphType<T> &graph, T item){
+    QueType<T> vertexQ;
+    graph.GetToVertices(item,vertexQ);
+    while (!vertexQ.IsEmpty())
+    {
+        vertexQ.Dequeue(item);
+        cout<<item<<' ';
+    }
+    cout<<endl;
+}
+
+int main()
+{
+      
+    #ifndef ONLINE_JUDGE  
+           freopen("input.txt", "r", stdin); 
+           freopen("output.txt", "w",stdout);
+    #endif
+    GraphType<int> graph;
+
+    graph.AddVertex(5);
+    graph.AddVertex(6);
+    graph.AddVertex(3);
+    graph.AddVertex(4);
+    graph.AddVertex(2);
+    graph.AddVertex(8);
+    graph.AddVertex(1);
+
+    //unordered graph
+    graph.AddEdge(5,6,100);    
+    graph.AddEdge(5,8,100); 
+    graph.AddEdge(6,2,100); 
+    graph.AddEdge(6,1,100); 
+    graph.AddEdge(3,8,100); 
+    graph.AddEdge(3,1,100);
+    graph.AddEdge(3,4,100); 
+    graph.AddEdge(4,2,100); 
+    graph.AddEdge(4,1,100);
+    cout<<"Adjacent vertices of vertex 3 : ";
+    printAdj(graph,3);
+    cout<<"Shortest path between from between 5 to 1 : ";
+    graph.custom_BFS(5,1);
+
+
+    return 0;
+}
